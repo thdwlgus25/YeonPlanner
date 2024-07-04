f226910958616d3ff5c9f09daff23bc901fc4ee4
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-07-05 01:01:20 +0900

    재연결

---
+++ src/main/java/com/yeonplanner/yeonplanner/YeonplannerApplication.java
@@ -0,0 +1,14 @@
+package com.yeonplanner.yeonplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeonplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeonplannerApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/yeonplanner/yeonplanner/YeonplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeonplanner.yeonplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeonplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
